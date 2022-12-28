47f7ed5489f2e3d9f206dbb48d327067423801dd
Light-3108 <119605030+Light-3108@users.noreply.github.com>
2022-12-28 22:52:28 +0545

    Update prefix_XorSUM.cpp

--- prefix_XorSUM.cpp
+++ prefix_XorSUM.cpp
@@ -1,37 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define endl '\n'
-
-#define ar array
-#define ll long long
-#define ld long double
-#define sza(x) ((int)x.size())
-#define all(a) (a).begin(), (a).end()
-
-#define PI 3.14159265358979323846264338327951
-const int MAX_N = 1e5 + 5;
-const ll INF = 1e9;
-const ld eps = 1e-6;
-const int N = 1e5 + 10;
-const int M = 1e9 + 7;
-ll lcm(ll a, ll b)
-{
-    return (a * b) / (__gcd(a, b));
-}
-void solve()
-{
-
-}
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int tc = 1;
-    cin >> tc;
-    for (int t = 1; t <= tc; t++)
+    vector<int> A{4, 2, 2, 6, 4};
+    int B = 6;
+    unordered_map<int, int> visited;
+    int cpx = 0;
+    long long c = 0;
+    for (int i = 0; i < A.size(); i++)
     {
-        solve();
+        cpx ^= A[i];
+        if (cpx == B)
+            c++;
+        int h = cpx ^ B;
+        if (visited.find(h) != visited.end())
+        {
+            c = c + visited[h];
+        }
+        visited[cpx]++;
     }
+    cout << c << endl;
     return 0;
 }
